b8a1413e2d2e4bc6abc2f5a87e8896495bd708bf
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-05 01:57:14 +0900

    Create Sales_by_Match.java

---
+++ HackerRank/Sales_by_Match.java
@@ -0,0 +1,70 @@
+/*
+
+There is a large pile of socks that must be paired by color. 
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+(https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup)
+
+2021-02-02
+
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int pair = 0;
+        int sameCnt = 1;
+        
+        Arrays.sort(ar);
+        
+        for (int cnt = 1; cnt < ar.length; cnt++) {
+            if (ar[cnt-1]==ar[cnt]) {
+                sameCnt++;
+                if (sameCnt%2==0) pair++;
+            } else {
+                sameCnt = 1;
+            }
+        }
+
+        return pair;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
