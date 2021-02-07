97030be1ceca05e5af36e23c4063d6aef7faafd0
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-08 01:23:28 +0900

    Create Sorting:Bubble_Sort.java

---
+++ HackerRank/Sorting:Bubble_Sort.java
@@ -0,0 +1,77 @@
+/*
+
+Consider the following version of Bubble Sort:
+
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+        }
+    }
+}
+
+Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. 
+Once sorted, print the following three lines:
+ 1. Array is sorted in numSwaps swaps., where numSwaps is the number of swaps that took place.
+ 2. First Element: firstElement, where firstElement is the first element in the sorted array.
+ 3. Last Element: lastElement, where lastElement is the last element in the sorted array.
+
+(https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting)
+
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int cntSwap = 0;
+        
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a.length-i-1; j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    
+                    cntSwap++;
+                }
+            }
+        }
+        
+        System.out.println("Array is sorted in " + cntSwap + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length-1]);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
