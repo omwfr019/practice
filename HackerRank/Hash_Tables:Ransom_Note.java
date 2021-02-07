55e139940d26ff381876ff9cb880f67fb2e1fe4a
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-07 19:53:10 +0900

    Create Hash_Tables:Ransom_Note.java

---
+++ HackerRank/Hash_Tables:Ransom_Note.java
@@ -0,0 +1,92 @@
+/*
+
+Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. 
+He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. 
+The words in his note are case-sensitive and he must use only whole words available in the magazine. 
+He cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+For example, the note is "Attack at dawn". The magazine contains only "attack at dawn". 
+The magazine has all the right words, but there's a case mismatch. The answer is .
+
+(https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps)
+
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+HashMap<String, Integer> convMagazine = new HashMap<String, Integer>();
+        int result = 0;
+        
+        for(String mWord : magazine) {
+            if (!convMagazine.containsKey(mWord)) {
+                convMagazine.put(mWord, 1);
+            } else {
+                convMagazine.put(mWord, convMagazine.get(mWord)+1);
+            }
+        }
+                
+        for(String nWord : note) {
+            if(!convMagazine.containsKey(nWord)) {
+                System.out.println("No");
+                return;
+            } else {
+                if (convMagazine.get(nWord) < 1) {
+                    System.out.println("No");
+                    return;
+                } else {
+                    convMagazine.put(nWord, convMagazine.get(nWord)-1);
+                }
+            }
+        }
+
+        System.out.println("Yes");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
