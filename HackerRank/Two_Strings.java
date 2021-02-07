b5678ff267ec4b88a66e98e34d6d90d1ccbf03ef
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-07 20:52:18 +0900

    Create Two_Strings.java

---
+++ HackerRank/Two_Strings.java
@@ -0,0 +1,61 @@
+/*
+
+Given two strings, determine if they share a common substring. 
+A substring may be as small as one character.
+
+(https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps)
+
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        HashSet<Character> s1Conv = new HashSet<Character>();
+        
+        for(int i=0; i<s1.length(); i++) {
+            s1Conv.add(s1.charAt(i));
+        }
+        
+        for(int j=0; j<s2.length(); j++) {
+            if(s1Conv.contains(s2.charAt(j))) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
