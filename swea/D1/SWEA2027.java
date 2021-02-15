3eacbe64433ee6dbe4321c8463f43df7706b0f0f
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-15 23:44:11 +0900

    Update SWEA2027.java

--- swea/D1/SWEA2027.java
+++ swea/D1/SWEA2027.java
@@ -1,18 +1,30 @@
 package swea.D1;
 
 // 2027. 대각선 출력하기
-public class SWEA2027 {
+class Solution {
 	public static void main(String[] args) throws Exception {
-		for(int line=0; line<5; line++) {
-			for(int i=0; i<line; i++) {
+		// Approach 1
+		for (int line = 0; line < 5; line++) {
+			for (int l = 0; l < line; l++) {
 				System.out.print("+");
 			}
 			System.out.print("#");
-			for(int k=4; k>line; k--) {
+			for (int r = 4; r > line; r--) {
 				System.out.print("+");
 			}
 			System.out.println();
 		}
-	}
 
+		// Approach 2
+		for (int row = 0; row < 5; row++) {
+			for (int col = 0; col < 5; col++) {
+				if (col == row) {
+					System.out.print("#");
+				} else {
+					System.out.print("+");
+				}
+			}
+			System.out.println();
+		}
+	}
 }
