51945124154903a60298d510a0446767a1039d8b
Eve <53002304+omwfr019@users.noreply.github.com>
2022-07-14 05:58:25 +0900

    Update FrequencyCounterTest.js

--- solving/FrequencyCounterTest.js
+++ solving/FrequencyCounterTest.js
@@ -5,7 +5,6 @@
 */
 
 
-// 1차
 function same(array1, array2) {
     let aryToMap = new Map();
 
@@ -40,8 +39,8 @@ function same(array1, array2) {
 
 
 /**
-  * EX) A naive solution (Time Commplexity = O(N^2))
-  */
+ * EX) A naive solution (Time Commplexity = O(N^2))
+ */
 function sameEx1(arr1, arr2) {
     if(arr1.length !== arr2.length) {
         return false; 
@@ -67,7 +66,21 @@ function sameEx2(arr1, arr2) {
   
     let frequencyCounter1 = {}
     let frequencyCounter2 = {}
-    
+    for(let val of arr1) {
+      frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2) {
+      frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+    for(let key in frequencyCounter1) {
+      if(!(key ** 2 in frequencyCounter2)) {
+          return false 
+      }
+      if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+          return false 
+      }
+      return true
+    }
 }
 
 
