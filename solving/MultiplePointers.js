37731aaa2c92b63ef43a41a39640477b8bf3f3d9
Eve <53002304+omwfr019@users.noreply.github.com>
2022-07-17 11:31:50 +0900

    Update MultiplePointers.js

--- solving/MultiplePointers.js
+++ solving/MultiplePointers.js
@@ -24,6 +24,41 @@ function sumZero2(arr) {
     }
 }
 
+/**
+ * Ex) Naive Solution
+ * Time Complexity = O(n^2)
+ * Space Complexity = O(1)
+ */
+function sumZeroEx1(arr) {
+    for(let i = 0; i < arr.lnegth; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            if(arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+/**
+ * Ex) Refactor
+ * Time Complexity = O(n)
+ * Space Complexity = (1)
+ */
+function sumZeroEx2(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left] + arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
 
 console.log(sumZero([-3,-2,-1,0,1,2,3])) //[-3, 3]
 console.log(sumZero([-2,0,1,3])) //undefined
